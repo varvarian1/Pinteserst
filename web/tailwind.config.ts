2f50f518e87bef16c38f6a8e77b25e27a77c0c86
Maksim <dymufa@gmail.com>
2024-06-01 13:06:28 +0300

    .

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			gap: {
+				10: '10px',
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic':
