70f1a65681f2bb230a538933c08953d20b98fb25
Varvar1an <varvarian1000@gmail.com>
2024-06-03 19:25:47 +0300

    fix rest

--- backend/project/rest/serializers.py
+++ backend/project/rest/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from registration.models import Registration
 from create.models import Photos
 
+from django.contrib.auth.models import User
+
 class PhotosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photos
@@ -11,3 +13,17 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = '__all__'
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
